279c1cbc7f453bf291090074b35a13d6944b4531
sanecz <lisa.diablo@gmail.com>
2014-12-25 00:20:50 +0100

    trying to perform a search... not working...

--- example.c
+++ example.c
@@ -14,6 +14,17 @@
  * <http://www.OpenLDAP.org/license.html>.
  */
 
+
+/* Howtos
+ *
+ * Sending error:
+ * op->o_bd->bd_info = (BackendInfo *)(on->on_info);
+ * send_ldap_error(op, rs, LDAP_INVALID_SYNTAX,
+ *   "text test error example_response()");
+ *  return(rs->sr_err);
+ *
+ */
+
 #include "portable.h"
 
 #ifdef SLAPD_OVER_EXAMPLE
@@ -30,12 +41,12 @@ typedef struct example_data {
 
 static ConfigTable examplecfg[] = {
   { "ExampleDomain", "arg", 2, 2, 0,
-    ARG_STRING|ARG_OFFSET, (void *)offsetof(example_data, principalattr),
+    ARG_STRING|ARG_OFFSET, (void *)offsetof(example_data, exampledomain),
     "( OLcfgCtAt:24.1 NAME 'ExampleDomain' "
     "DESC 'Example domain' "
     "SYNTAX OMsDirectoryString SINGLE-VALUE )", NULL, NULL},
   { "PrincipalAttr", "arg", 2, 2, 0,
-    ARG_STRING|ARG_OFFSET, (void *)offsetof(example_data, exampledomain),
+    ARG_STRING|ARG_OFFSET, (void *)offsetof(example_data, principalattr),
     "( OLcfgCtAt:24.2 NAME 'PrincipalAttr' "
     "DESC 'Principal Attr' "
     "SYNTAX OMsDirectoryString SINGLE-VALUE )", NULL, NULL },
@@ -84,6 +95,43 @@ static int example_add(Operation *op, SlapReply *rs) {
   return SLAP_CB_CONTINUE;
 }
 
+static int example_search(Operation *op,
+			  Operation *nop,
+			  SlapReply *rs,
+			  struct berval *bvkey) {
+  SlapReply nrs = { REP_RESULT };
+  slap_overinst *on = (slap_overinst *) op->o_bd->bd_info;
+  int rc;
+
+  nop->ors_filter = str2filter_x(nop, bvkey->bv_val);
+  if(nop->ors_filter == NULL) {
+    op->o_bd->bd_info = (BackendInfo *) on->on_info;
+    send_ldap_error(op, rs, LDAP_OTHER,
+		    "invalid filter");
+    return(rs->sr_err);
+  }
+  nop->ors_filterstr = *bvkey;
+  nop->o_tag = LDAP_REQ_SEARCH;
+  nop->ors_scope = LDAP_SCOPE_BASE;
+  nop->ors_deref = LDAP_DEREF_NEVER;
+  nop->ors_limit = NULL;
+  nop->ors_slimit = SLAP_NO_LIMIT;
+  nop->ors_tlimit = SLAP_NO_LIMIT;
+  nop->ors_attrsonly = 1;
+  nop->ors_attrs  = slap_anlist_no_attrs;
+
+  nop->o_bd = on->on_info->oi_origdb;
+  rc = nop->o_bd->be_search(nop, &nrs);
+  filter_free_x(nop, nop->ors_filter, 1);
+  op->o_tmpfree(bvkey->bv_val, op->o_tmpmemctx);
+  if(rc != LDAP_SUCCESS && rc != LDAP_NO_SUCH_OBJECT) {
+    op->o_bd->bd_info = (BackendInfo *) on->on_info;
+    send_ldap_error(op, rs, rc, "failed");
+    return(rs->sr_err);
+  }
+  return SLAP_CB_CONTINUE;
+}
+
 static int example_response(Operation *op, SlapReply *rs) {
   slap_overinst *on = (slap_overinst *)op->o_bd->bd_info;
   example_data *ex = on->on_bi.bi_private;
@@ -98,7 +146,17 @@ static int example_response(Operation *op, SlapReply *rs) {
   case LDAP_REQ_ADD:
     printf("ldap req add case\n");
     for(a = op->ora_e->e_attrs; a; a = a->a_next) {
-      printf("%s: %s\n", a->a_desc->ad_cname.bv_val, a->a_vals->bv_val);
+      if (!strcmp(a->a_desc->ad_cname.bv_val, ex->principalattr)) {
+	Operation nop = *op;
+	struct berval bvkey;
+
+	bvkey.bv_val = strdup("cn=test1");
+	bvkey.bv_len = (ber_len_t)strlen(bvkey.bv_val);
+	if (example_search(op, &nop, rs, &bvkey) != SLAP_CB_CONTINUE) {
+	  printf("4\n");
+	  return (rs->sr_err);
+	}
+      }
     }
     break;
   default:
